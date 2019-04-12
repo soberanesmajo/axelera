fef524b7d4d0f5902457b7e1f8a04104068b90d6
soberanesmajo <soberanesmajo@gmail.com>
2019-04-12 16:48:34 -0500

    add PersonalCards component

--- src/components/Personal.vue
+++ src/components/Personal.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
 		<PersonalImagen/>
+		<PersonalCards/>
 	</div>
 </template>
 
 <script>
 import PersonalImagen from './PersonalImagen.vue';
+import PersonalCards from './PersonalCards.vue';
 
 export default {
   name: 'Personal',
 	components: {
-		PersonalImagen
+		PersonalImagen,
+		PersonalCards
 	}
 }
 </script>
