a7527b7b7476ff2e33d987afb46d9269233bfb0f
soberanesmajo <soberanesmajo@gmail.com>
2019-05-15 18:04:57 -0500

    updating forms. Last commit of Majo <3

--- src/components/Contrasena/FormContrasena.vue
+++ src/components/Contrasena/FormContrasena.vue
@@ -36,10 +36,11 @@ export default {
 		text-align: justify;
 	}
 	.form {
-		background-color: white;
 		width: 25rem;
 		height: 22rem;
-		border-radius: 15px;
+		border-radius: 5px;
+		box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); 
+		margin-top: 8rem;
 	}
 	.header {
 		text-align: center;
